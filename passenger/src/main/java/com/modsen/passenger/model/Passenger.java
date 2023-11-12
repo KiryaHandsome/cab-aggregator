4db87889b346fddef4d17d278ed4a31165f11156
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-11-12 12:39:00 +0300

    feat: add passenger service

---
+++ passenger/src/main/java/com/modsen/passenger/model/Passenger.java
@@ -0,0 +1,29 @@
+package com.modsen.passenger.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "passengers")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
