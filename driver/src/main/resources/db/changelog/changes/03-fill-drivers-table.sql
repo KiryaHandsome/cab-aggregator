25f382093bb3dc5ebe2a6ed2eeafe96b33b94648
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-11-16 22:57:45 +0300

    feat: add migration for inserting data to drivers table

---
+++ driver/src/main/resources/db/changelog/changes/03-fill-drivers-table.sql
@@ -0,0 +1,11 @@
+INSERT INTO drivers (name, surname, email, phone_number, status)
+VALUES ('John', 'Doe', 'john.doe@example.com', '1234567890', 'Active'),
+       ('Jane', 'Smith', 'jane.smith@example.com', '9876543210', 'Inactive'),
+       ('Alice', 'Johnson', 'alice.johnson@example.com', '5555555555', 'Active'),
+       ('Bob', 'Williams', 'bob.williams@example.com', '1111111111', 'Active'),
+       ('Eva', 'Brown', 'eva.brown@example.com', '9999999999', 'Inactive'),
+       ('Michael', 'Lee', 'michael.lee@example.com', '7777777777', 'Active'),
+       ('Olivia', 'Miller', 'olivia.miller@example.com', '8888888888', 'Inactive'),
+       ('David', 'Davis', 'david.davis@example.com', '4444444444', 'Active'),
+       ('Sophia', 'Taylor', 'sophia.taylor@example.com', '3333333333', 'Inactive'),
+       ('Matthew', 'Moore', 'matthew.moore@example.com', '6666666666', 'Active');
