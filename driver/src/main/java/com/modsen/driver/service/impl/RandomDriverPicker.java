495d1b0cedaf940665dc22d1a6b293a73c8a02a5
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-11-16 23:34:24 +0300

    fix: add driver status update on ride events

--- driver/src/main/java/com/modsen/driver/service/impl/RandomDriverPicker.java
+++ driver/src/main/java/com/modsen/driver/service/impl/RandomDriverPicker.java
@@ -4,10 +4,10 @@ import com.modsen.driver.dto.response.DriverResponse;
 import com.modsen.driver.exception.NoAvailableDriversException;
 import com.modsen.driver.mapper.DriverMapper;
 import com.modsen.driver.model.Driver;
+import com.modsen.driver.model.Status;
 import com.modsen.driver.repository.DriverRepository;
 import com.modsen.driver.service.DriverPicker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -27,13 +27,12 @@ public class RandomDriverPicker implements DriverPicker {
             retryFor = NoAvailableDriversException.class
     )
     public DriverResponse pickAvailable() {
-        Page<Driver> driver = driverRepository.findAll(Pageable.ofSize(1));
-        if (driver.isEmpty()) {
-            throw new NoAvailableDriversException("Driver not found");
-        }
-        return driver.map(driverMapper::toResponse)
+        Driver driver = driverRepository.findAll(Pageable.ofSize(1))
                 .stream()
                 .findFirst()
-                .get();
+                .orElseThrow(() -> new NoAvailableDriversException("Driver not found"));
+        driver.setStatus(Status.BUSY);
+        driverRepository.save(driver);
+        return driverMapper.toResponse(driver);
     }
 }
--- driver/src/main/java/com/modsen/driver/service/impl/RideExecutorStub.java
+++ driver/src/main/java/com/modsen/driver/service/impl/RideExecutorStub.java
@@ -1,6 +1,9 @@
 package com.modsen.driver.service.impl;
 
 import com.modsen.driver.dto.event.RideEvent;
+import com.modsen.driver.dto.request.DriverUpdate;
+import com.modsen.driver.model.Status;
+import com.modsen.driver.service.DriverService;
 import com.modsen.driver.service.RideExecutor;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -12,11 +15,16 @@ import org.springframework.stereotype.Service;
 public class RideExecutorStub implements RideExecutor {
 
     private final KafkaProducer kafkaProducer;
+    private final DriverService driverService;
 
     @Async
     @SneakyThrows
     public void ride(RideEvent event) {
         Thread.sleep(5000);
+        DriverUpdate request = DriverUpdate.builder()
+                .status(Status.AVAILABLE)
+                .build();
+        driverService.update(event.getDriverId(), request);
         kafkaProducer.sendMessage("ride-ended", event);
     }
 }
