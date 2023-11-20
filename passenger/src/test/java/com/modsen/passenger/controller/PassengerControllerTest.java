f0e3c44506a610c39ae2388114fab355e1684a21
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-11-20 12:05:35 +0300

    test: add fail test to check CI

---
+++ passenger/src/test/java/com/modsen/passenger/controller/PassengerControllerTest.java
@@ -0,0 +1,13 @@
+package com.modsen.passenger.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassengerControllerTest {
+
+    @Test
+    void failTest() {
+        assertTrue(false);
+    }
+}
