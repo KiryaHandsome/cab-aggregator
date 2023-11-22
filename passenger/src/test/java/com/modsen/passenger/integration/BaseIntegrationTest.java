ca15d5d4a9cf4d61b936b578e1c98dd5e74d5fb9
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-11-23 02:23:31 +0300

    fix: fix postgres container in BaseIT

--- passenger/src/test/java/com/modsen/passenger/integration/BaseIntegrationTest.java
+++ passenger/src/test/java/com/modsen/passenger/integration/BaseIntegrationTest.java
@@ -4,7 +4,6 @@ package com.modsen.passenger.integration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,10 +12,12 @@ public class BaseIntegrationTest {
 
     public static final String POSTGRES_IMAGE_NAME = "postgres:15-alpine";
 
-    @Container
-    protected static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
-            DockerImageName.parse(POSTGRES_IMAGE_NAME)
-    );
+    protected static final PostgreSQLContainer<?> postgresContainer;
+
+    static {
+        postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_IMAGE_NAME));
+        postgresContainer.start();
+    }
 
     @DynamicPropertySource
     static void init(DynamicPropertyRegistry registry) {
