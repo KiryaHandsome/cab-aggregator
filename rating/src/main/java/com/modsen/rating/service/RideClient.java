f419e9fc74e48bc821b161ae5de4694fe56c9dac
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-12-14 15:35:32 +0300

    refactor: fix review comments

--- rating/src/main/java/com/modsen/rating/service/RideClient.java
+++ rating/src/main/java/com/modsen/rating/service/RideClient.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("${ride.name}")
+@FeignClient(value = "${ride.name}", path = "${ride.path}")
 public interface RideClient {
 
-    @GetMapping("/api/v1/rides/check-shared-ride")
+    @GetMapping("/check-shared-ride")
     SharedRideResponse haveSharedRide(@RequestParam("driver-id") Integer driverId,
                                       @RequestParam("passenger-id") Integer passengerId);
 }
