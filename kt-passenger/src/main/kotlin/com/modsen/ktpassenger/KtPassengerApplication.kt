5eac0b6ae4f769b71e7009857e419d369992eb2f
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-12-19 14:08:57 +0300

    feat: add eureka client

--- kt-passenger/src/main/kotlin/com/modsen/ktpassenger/KtPassengerApplication.kt
+++ kt-passenger/src/main/kotlin/com/modsen/ktpassenger/KtPassengerApplication.kt
@@ -2,7 +2,9 @@ package com.modsen.ktpassenger
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class KtPassengerApplication
 
